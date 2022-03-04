4a6e6a8d934248277947b38b08ceccc028c2a069
catalinajitarasu <79159393+catalinajitarasu@users.noreply.github.com>
2022-03-04 22:43:34 +0200

    remove Laborator2-Problema2

--- Laborator2-Problema2/Lab2problema2/Lab2problema2/Sort.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Student.h"
-
-int sortName(Student* student1, Student* student2);
-int sortMate(Student* grade1, Student* grade2);
-int sortIstorie(Student* grade1, Student* grade2);
-int sortEngleza(Student* grade1, Student* grade2);
-int sortMedie(Student* medie1, Student* medie2);
